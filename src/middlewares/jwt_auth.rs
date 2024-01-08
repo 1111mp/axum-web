6b51e7b5b77215ca9ae843e1f373d456298a5201
The1111mp <The1111mp@outlook.com>
2024-01-08 10:23:45 +0800

    refactor: jwt_auth
    Signed-off-by: The1111mp <The1111mp@outlook.com>

--- src/middlewares/jwt_auth.rs
+++ src/middlewares/jwt_auth.rs
@@ -1,44 +1,68 @@
+use std::env;
+
 use axum::{
     extract::{Request, State},
     http::header,
     middleware::Next,
-    response::Response,
+    response::{IntoResponse, Response},
 };
+use redis::AsyncCommands;
 
-use crate::{routes::AppState, utils::http_resp::JsonResponse};
+use crate::{
+    routes::AppState,
+    utils::{exception::KnownError, http_resp::JsonResponse},
+};
 
 use super::current_user::authorize_current_user;
 
 // usage: https://docs.rs/axum/latest/axum/middleware/fn.from_fn_with_state.html
 pub async fn auth_guard(
-    State(state): State<AppState>,
+    State(mut state): State<AppState>,
     // you can add more extractors here but the last
     // extractor must implement `FromRequest` which
     // `Request` does
     mut req: Request,
     next: Next,
-) -> Result<Response, JsonResponse<()>> {
-    let token = req
-        .headers()
-        .get(header::AUTHORIZATION)
-        .and_then(|token| token.to_str().ok());
-
-    let token = if let Some(token) = token {
-        token
+) -> Result<Response, KnownError> {
+    let headers = req.headers();
+    // userid
+    let user_id = req.headers().get("userid").and_then(|id| id.to_str().ok());
+    let user_id = if let Some(user_id) = user_id {
+        user_id
     } else {
-        return Err(JsonResponse::Unauthorized {
+        return Ok(JsonResponse::<()>::Unauthorized {
             message: "Unauthorized".to_string(),
-        });
+        }
+        .into_response());
+    };
+    // authorization
+    let token_key = headers
+        .get(header::AUTHORIZATION)
+        .and_then(|token_key| token_key.to_str().ok());
+    let token_key = if let Some(token_key) = token_key {
+        token_key
+    } else {
+        return Ok(JsonResponse::<()>::Unauthorized {
+            message: "Unauthorized".to_string(),
+        }
+        .into_response());
     };
 
-    if let Ok(current_user) = authorize_current_user(token).await {
+    let app_auth_key = env::var("APP_AUTH_KEY").unwrap_or("app_auth_key".to_string());
+    let key = format!("{app_auth_key}_{user_id}");
+    let token_value: String = state.redis.hget(&key, token_key).await?;
+
+    if let Ok(current_user) = authorize_current_user(&token_value).await {
+        // refresh token expiration time. (1 hour)
+        state.redis.expire(&key, 60 * 60).await?;
         // insert the current user into a request extension so the handler can
         // extract it
         req.extensions_mut().insert(current_user);
         Ok(next.run(req).await)
     } else {
-        Err(JsonResponse::Unauthorized {
+        Ok(JsonResponse::<()>::Unauthorized {
             message: "Unauthorized".to_string(),
-        })
+        }
+        .into_response())
     }
 }
--- src/utils/http_resp.rs
+++ src/utils/http_resp.rs
@@ -7,6 +7,41 @@ use serde::Serialize;
 use serde_json::json;
 
 #[derive(Debug)]
+pub struct MyResponse<T: Serialize> {
+    pub code: StatusCode,
+    pub message: String,
+    pub data: Option<T>,
+}
+
+impl<T: Serialize> From<MyResponse<T>> for Response {
+    fn from(inner: MyResponse<T>) -> Self {
+        (
+            inner.code,
+            Json(json!({
+                "code": inner.code.as_u16(),
+                "message": inner.message,
+                "data": inner.data
+            })),
+        )
+            .into_response()
+    }
+}
+
+impl<T: Serialize> IntoResponse for MyResponse<T> {
+    fn into_response(self) -> Response {
+        (
+            self.code,
+            Json(json!({
+                "code": self.code.as_u16(),
+                "message": self.message,
+                "data": self.data
+            })),
+        )
+            .into_response()
+    }
+}
+
+#[derive(Debug)]
 pub enum JsonResponse<T: Serialize> {
     OK { message: String, data: Option<T> },
 
@@ -17,6 +52,67 @@ pub enum JsonResponse<T: Serialize> {
     NotFound { message: String },
 
     InternalServerError { message: String },
+}
+
+impl<T: Serialize> From<JsonResponse<T>> for Response {
+    fn from(json_response: JsonResponse<T>) -> Self {
+        match json_response {
+            JsonResponse::OK { message, data } => match data {
+                Some(data) => (
+                    StatusCode::OK,
+                    Json(json!({
+                      "code": 200,
+                      "messsage": message,
+                      "data": data
+                    })),
+                ),
+                None => (
+                    StatusCode::OK,
+                    Json(json!({
+                      "code": 200,
+                      "messsage": message,
+                    })),
+                ),
+            }
+            .into_response(),
+
+            JsonResponse::RedirectTo { uri } => Redirect::to(uri.as_str()).into_response(),
+
+            JsonResponse::BadRequest { message } => (
+                StatusCode::BAD_REQUEST,
+                Json(json!({
+                  "code": 400,
+                  "message": message
+                })),
+            )
+                .into_response(),
+            JsonResponse::Unauthorized { message } => (
+                StatusCode::UNAUTHORIZED,
+                Json(json!({
+                  "code": 401,
+                  "message": message
+                })),
+            )
+                .into_response(),
+            JsonResponse::NotFound { message } => (
+                StatusCode::NOT_FOUND,
+                Json(json!({
+                  "code": 404,
+                  "message": message
+                })),
+            )
+                .into_response(),
+
+            JsonResponse::InternalServerError { message } => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({
+                  "code": 500,
+                  "message": message
+                })),
+            )
+                .into_response(),
+        }
+    }
 }
 
 impl<T: Serialize> IntoResponse for JsonResponse<T> {
